95c7b5486e548bb977a6cdece48e3f8ac121475f
Jittojoyes98 <jittojoyes77@gmail.com>
2023-12-29 09:54:21 +0530

    Add: functions

---
+++ functions/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main()  {
+	fmt.Println("Heey")
+	var ans int=adder(1,3)
+	println(ans) 
+	var result int = calculate(1,2,3,5)
+	println(result)
+}
+
+// specify the types and return type
+func adder(a int,b int) int {
+	return a+b
+}
+
+func calculate(sumarray ...int) int  {
+	total:=0
+	for _,val := range(sumarray){
+		total+=val
+	}
+	return total
+}
