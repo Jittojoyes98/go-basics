db69c2daf469e27efb2c38363178a43dee1ad419
Jitto Joyes <jittojoyes77@gmail.com>
2024-01-01 12:07:53 +0530

    add: read files.

--- files/main.go
+++ files/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
 )
 
@@ -11,8 +12,21 @@ func main()  {
 	handleError(err)
 	const content string="Hey there new file is here."
 	length,err:=file.WriteString(content)
+	// here you can write like :
+	// os.WriteString(file,content)
 	handleError(err)
 	fmt.Print("The file length is \n",length)
+
+	// Read files using ioutil 
+	readFile("./createdFile.txt")
+}
+
+func readFile(filePath string)  {
+	databyte,err:=ioutil.ReadFile(filePath)
+	handleError(err)
+	print("\n")
+	fmt.Println("Text data in the file:",string(databyte))
+
 }
 
 func handleError(errorString error)  {
