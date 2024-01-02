5b5984e8bf90d65a9de7384af582b30b80ccb3e0
Jitto Joyes <jittojoyes77@gmail.com>
2024-01-02 13:49:38 +0530

    add : url package .

--- webrequest/main.go
+++ webrequest/main.go
@@ -4,13 +4,14 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 )
 
-const url="https://lco.dev"
+const requestUrl="https://lco.dev"
 
 func main()  {
 	fmt.Println("LCO request starts here..")
-	response,err:=http.Get(url)
+	response,err:=http.Get(requestUrl)
 	if err!=nil{
 		panic(err)
 	} 
@@ -23,4 +24,19 @@ func main()  {
 		panic(err)
 	} 
 	fmt.Println("The response data here is nice and easy ", string(databytes))
+
+
+	// usage of url package here.
+	
+	u,err:=url.Parse("http://bing.com/search?q=dotnet")
+	if err!=nil{
+		panic(err)
+	}
+	
+	fmt.Println("Curent scheme is ", u.Scheme)
+	u.Host="google.com"
+	q:=u.Query()
+	q.Set("q","golang") // changing the query here.
+	fmt.Println("The query is ",q)
+	fmt.Println("the updated url",u)
 }
